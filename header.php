c1843bff7edb0932fdce3c8d1a8d7afd6a37601b
Arrow-07 <nonnoluigi46@gmail.com>
2024-07-13 13:25:54 +0200

    13 07 24 1.1

--- header.php
+++ header.php
@@ -10,15 +10,42 @@ $colore4 = $colors['color4'];
 $colore5 = $colors['color5'];
 ?>
 
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 <style>
-   ul {background-color: <?php echo $colore4?>;}
-   a {color: <?php echo $colore1 ?>;}
+    ul {
+        background-color: <?php echo $colore4; ?>;
+        list-style-type: none;
+        padding: 10px;
+        margin: 0;
+        display: flex;
+        align-items: center;
+        border-radius: 12px;
+    }
+    li {
+        display: inline;
+        margin-right: 20px;
+    }
+    li:last-child {
+        margin-left: auto;
+    }
+    a {
+        color: <?php echo $colore1; ?>;
+        text-decoration: none;
+        font-family: 'Roboto', sans-serif;
+        font-size: 18px;
+        display: flex;
+        align-items: center;
+    }
+    a i {
+        margin-right: 8px;
+    }
 </style>
 
 <ul>
-    <li><a href="classifica.php">CLASSIFICA</a></li>
-    <li><a href="informazioni.php">INFORMAZIONI</a></li>
-    <li><a href="impostazioni.php">IMPOSTAZIONI</a></li>
-    <li><a href="accesso.php">ACCEDI</a></li>
+    <li><a href="index.php"><i class="fas fa-home"></i>HOME</a></li>
+    <li><a href="classifica.php"><i class="fas fa-trophy"></i> CLASSIFICA</a></li>
+    <li><a href="informazioni.php"><i class="fas fa-info-circle"></i> INFORMAZIONI</a></li>
+    <li><a href="impostazioni.php"><i class="fas fa-cog"></i> IMPOSTAZIONI</a></li>
+    <li><a href="accesso.php"><i class="fas fa-sign-in-alt"></i> ACCEDI</a></li>
 </ul>
--- index.php
+++ index.php
@@ -1,14 +1,18 @@
-
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HOME</title>
 </head>
+
 <body>
   <?php
   require('header.php');
   ?>
-  
+  <img src="./img/logo.png" alt="Logo OratorioScore">
+  <div class="vertical-line"></div>
+  <h1>Benvenuti!</h1>
 </body>
+
 </html>
