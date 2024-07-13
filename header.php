1c358a2024f751d163dfece68bbe24e20e1e107f
Arrow-07 <nonnoluigi46@gmail.com>
2024-07-13 14:28:10 +0200

    13 07 24 2.0

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ $colore5 = $colors['color5'];
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 <style>
-    ul {
+    #ulhead {
         background-color: <?php echo $colore4; ?>;
         list-style-type: none;
         padding: 10px;
@@ -22,14 +22,14 @@ $colore5 = $colors['color5'];
         align-items: center;
         border-radius: 12px;
     }
-    li {
+    #lihead {
         display: inline;
         margin-right: 20px;
     }
-    li:last-child {
+    #lihead:last-child {
         margin-left: auto;
     }
-    a {
+    #linkhead {
         color: <?php echo $colore1; ?>;
         text-decoration: none;
         font-family: 'Roboto', sans-serif;
@@ -37,15 +37,15 @@ $colore5 = $colors['color5'];
         display: flex;
         align-items: center;
     }
-    a i {
+    #linkhead #iconehead {
         margin-right: 8px;
     }
 </style>
 
-<ul>
-    <li><a href="index.php"><i class="fas fa-home"></i>HOME</a></li>
-    <li><a href="classifica.php"><i class="fas fa-trophy"></i> CLASSIFICA</a></li>
-    <li><a href="informazioni.php"><i class="fas fa-info-circle"></i> INFORMAZIONI</a></li>
-    <li><a href="impostazioni.php"><i class="fas fa-cog"></i> IMPOSTAZIONI</a></li>
-    <li><a href="accesso.php"><i class="fas fa-sign-in-alt"></i> ACCEDI</a></li>
+<ul id="ulhead">
+    <li id="lihead"><a id="linkhead" href="index.php">          <i id="iconehead" class="fas fa-home"></i>HOME</a></li>
+    <li id="lihead"><a id="linkhead" href="classifica.php">     <i id="iconehead" class="fas fa-trophy"></i> CLASSIFICA</a></li>
+    <li id="lihead"><a id="linkhead" href="informazioni.php">   <i id="iconehead" class="fas fa-info-circle"></i> INFORMAZIONI</a></li>
+    <li id="lihead"><a id="linkhead" href="gestisci.php">       <i id="iconehead" class="fas fa-cog"></i> GESTIONE</a></li>
+    <li id="lihead"><a id="linkhead" href="accesso.php">        <i id="iconehead" class="fas fa-sign-in-alt"></i> ACCEDI</a></li>
 </ul>
